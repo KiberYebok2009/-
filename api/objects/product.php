2de5cd25578cbfd1bc9eec85eabfc4c23e04079a
Vlad Noskov <inoskoff@me.com>
2024-04-20 20:11:30 +0000

    Добавление create в class Product Реализовано доболвние продукта в базу данных

--- api/objects/product.php
+++ api/objects/product.php
@@ -78,40 +78,27 @@
             $this->category_id = $row['category_id'];
         }
     
-        // Метод для создания продукта
-        function create() {
-            // Запрос SQL для вставки записи
-            $query = "INSERT INTO " . $this->table_name . "
-                    SET
-                        name=:name, description=:description, price=:price, category_id=:category_id, created=:created, path_photo=:path_photo";
-    
-            // Подготовка запроса
+        // Метод для создания нового товара
+        public function create() {
+            $query = "INSERT INTO {$this->table_name} (name, description, price, path_photo, category_id) VALUES (:name, :description, :price, :path_photo, :category_id)";
+        
             $stmt = $this->conn->prepare($query);
-    
-            // Очистка и привязка данных
-            $this->name = htmlspecialchars(strip_tags($this->name));
-            $this->description = htmlspecialchars(strip_tags($this->description));
-            $this->price = htmlspecialchars(strip_tags($this->price));
-            $this->category_id = htmlspecialchars(strip_tags($this->category_id));
-            $this->created = htmlspecialchars(strip_tags($this->created));
-            $this->path_photo = htmlspecialchars(strip_tags($this->path_photo)); // Очистка нового поля path_photo
-    
-            // Привязка значений
-            $stmt->bindParam(":name", $this->name);
-            $stmt->bindParam(":description", $this->description);
-            $stmt->bindParam(":price", $this->price);
-            $stmt->bindParam(":category_id", $this->category_id);
-            $stmt->bindParam(":created", $this->created);
-            $stmt->bindParam(":path_photo", $this->path_photo); // Привязка нового поля path_photo
-    
-            // Выполнение запроса
-            if($stmt->execute()) {
+        
+            // очистка и привязка значений
+            $stmt->bindParam(':name', htmlspecialchars(strip_tags($this->name)));
+            $stmt->bindParam(':description', htmlspecialchars(strip_tags($this->description)));
+            $stmt->bindParam(':price', htmlspecialchars(strip_tags($this->price)));
+            $stmt->bindParam(':path_photo', htmlspecialchars(strip_tags($this->path_photo)));
+            $stmt->bindParam(':category_id', htmlspecialchars(strip_tags($this->category_id)));
+        
+            // выполнение запроса
+            if ($stmt->execute()) {
                 return true;
             }
-    
+        
             return false;
         }
-    
+        
         // Методы update и delete могут быть реализованы аналогично, включая обработку поля path_photo
         public function update_product() {
             $query = "UPDATE {$this->table_name}
